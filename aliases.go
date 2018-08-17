9dc4f448ba3aaafc1c0ea8c0f61846eb717dd77f
Moises P. Sena <moisespsena@gmail.com>
2018-08-17 11:02:57 -0300

    Format

--- aliases.go
+++ aliases.go
@@ -2,10 +2,15 @@ package pluggable
 
 import "github.com/moisespsena/go-edis"
 
-type EventDispatcherInterface = edis.EventDispatcherInterface
-type EventInterface = edis.EventInterface
-type Event = edis.Event
-type CallbackFunc = edis.CallbackFunc
-type CallbackFuncE = edis.CallbackFuncE
+type (
+	EventDispatcherInterface = edis.EventDispatcherInterface
+	EventInterface           = edis.EventInterface
+	Event                    = edis.Event
+	CallbackFunc             = edis.CallbackFunc
+	CallbackFuncE            = edis.CallbackFuncE
+)
 
-var EAll = edis.EAll
+var (
+	EAll     = edis.EAll
+	NewEvent = edis.NewEvent
+)
