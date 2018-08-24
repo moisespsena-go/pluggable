4b32eb3244169656ba570b38b81418304a82811e
Moises P. Sena <moisespsena@gmail.com>
2018-08-24 11:38:10 -0300

    Added AssetFS plugin event

--- assetfs.go
+++ assetfs.go
@@ -3,8 +3,14 @@ package pluggable
 import (
 	"path"
 
+	"reflect"
+
 	"github.com/moisespsena/go-assetfs/api"
+	"github.com/moisespsena/go-error-wrap"
+	"github.com/moisespsena/go-path-helpers"
 )
+
+const E_ASSET_FS = "pluggable.AssetFS"
 
 type PluginFSInterface interface {
 	EventDispatcherInterface
@@ -31,6 +37,7 @@ func NewPluginsFS(fs api.Interface) *PluginsFS {
 	pls.SetDispatcher(pls)
 	pls.pathRegisterFunc = DefaultFSPathRegister
 	pls.OnPlugin("register", func(e PluginEventInterface) error {
+		register := pls.Dispatcher().(PluginFSInterface).AssetFSPathRegister()
 		p := e.Plugin()
 		if p.AbsPath != "" {
 			if assetsPath, ok := p.Value.(PluginAssetsRootPath); ok {
@@ -43,7 +50,15 @@ func NewPluginsFS(fs api.Interface) *PluginsFS {
 			if ns, ok := p.Value.(PluginFSNameSpace); ok {
 				p.NameSpace = ns.NameSpace()
 			}
-			pls.Dispatcher().(PluginFSInterface).AssetFSPathRegister()(pls.AssetFS, path.Join(p.AssetsRoot, "assets"))
+			register(pls.AssetFS, path.Join(p.AssetsRoot, "assets"))
+		}
+
+		if dis, ok := p.Value.(EventDispatcherInterface); ok {
+			e := &AssetFSEvent{NewPluginEvent(E_ASSET_FS), fs, register}
+			e.SetPlugin(p)
+			if err := dis.Trigger(e); err != nil {
+				return errwrap.Wrap(err, "Trigger ", E_ASSET_FS)
+			}
 		}
 		return nil
 	})
@@ -52,4 +67,30 @@ func NewPluginsFS(fs api.Interface) *PluginsFS {
 
 func DefaultFSPathRegister(fs api.Interface, pth string) error {
 	return fs.RegisterPath(pth)
+}
+
+type AssetFSEvent struct {
+	PluginEventInterface
+	AssetFS  api.Interface
+	Register func(fs api.Interface, pth string) error
+}
+
+func (AssetFSEvent) PathOf(value interface{}) (pth string) {
+	t := reflect.TypeOf(value)
+	for t.Kind() != reflect.Struct {
+		t = t.Elem()
+	}
+	pth = t.PkgPath()
+	pth = path_helpers.ResolveGoSrcPath(pth)
+	return
+}
+
+func (a *AssetFSEvent) RegisterAssets(basePath string) {
+	a.Register(a.AssetFS, path.Join(basePath, "assets"))
+}
+
+func OnAssetFS(p EventDispatcherInterface, cb func(e *AssetFSEvent)) {
+	p.On(E_ASSET_FS, func(e PluginEventInterface) {
+		cb(e.(*AssetFSEvent))
+	})
 }
